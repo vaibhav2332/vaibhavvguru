2beb50756ffeab8ba83232dd633307d8f2a6e9c8
vaibhav2332 <vaibhavprajapati369@gmail.com>
2025-05-22 11:14:29 +0530

    Update type.py

--- modules/type.py
+++ modules/type.py
@@ -8,17 +8,20 @@ from pyrogram.types import Message
 from utils.misc import modules_help, prefix
 
 
-@Client.on_message(filters.command(["type2", "typewriter2"], prefix) & filters.me)
+@Client.on_message(filters.command(["type", "typewriter"], prefix) & filters.me)
 async def type_cmd(_, message: Message):
     text = message.text.split(maxsplit=1)[1]
     typed = ""
+    typing_symbol = "▒"
 
     for char in text:
+        await message.edit(typed + typing_symbol)
+        await asyncio.sleep(0.05)
         typed += char
         await message.edit(typed)
-        await asyncio.sleep(0.01)
+        await asyncio.sleep(0.05)
 
 
-modules_help["type2"] = {
-    "type2</code> <code>| </code><code>typewriter [text]*": "Typing emulation. Don't use a lot of characters, you can receive a lot of floodwaits!"
+modules_help["type"] = {
+    "type</code> <code>| </code><code>typewriter [text]*": "Typing emulation. Don't use a lot of characters, you can receive a lot of floodwaits!"
 }
